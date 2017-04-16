502d4377bf270260905951f9b73498749dabaa49
Dan <email@danielhand.io>
2017-04-16 09:19:46 +0100

    Update wc_import.sh

--- wc_import.sh
+++ wc_import.sh
@@ -4,3 +4,13 @@ while IFS='|' read -r images product_title short_description category weight reg
 do
 wp media import "${images}" --featured_image  --post_id=$(wp wc product create --name="${product_title}" --short_description='${short_description}' --categories='${category}' --weight="${weight}" --regular_price="${regular_price}"  --stock_quantity="${stock_qty}" --manage_stock="${manage_stock}" --allow-root --user=danielhand | awk '{print $4}') --user=danielhand --allow-root
 done < product-new.txt
+
+
+
+###### NOTES ######
+
+# Dimensions can be imported by using --dimensions='{"length":"100","height":"100","width":"100"}' however, this causes issues when pulling the values from the CSV as variables need to be enclosed in " ". Need to try and change the way the values are displayed from the CSV.
+
+# STILL NEED TO DO
+# Gallery images
+# Categories
